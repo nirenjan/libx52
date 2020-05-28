f9be0b3172b04db8e3da5e10019cd45cbf4007fc
nirenjan <nirenjan@gmail.com>
2020-05-28 15:31:08 -0700

    Move x52cli documentation into Doxygen
    Prior to this change, the x52cli man page was a manually created file
    that was out of date with the source. Rather than update that file, and
    maintain a separate document for the HTML sources, it makes sense to
    consolidate both the HTML and manpage documentation to a Doxygen page
    block within the source and update the Doxyfile to generate man pages as
    well.
    
    This change also removes the obsolete manually maintained manpage, and
    falls back to Doxygen to build and install the manpage.

--- utils/cli/x52_cli.c
+++ utils/cli/x52_cli.c
@@ -6,6 +6,142 @@
  * SPDX-License-Identifier: GPL-2.0-only WITH Classpath-exception-2.0
  */
 
+/**
+@page x52cli Command Line Interface to libx52
+
+\htmlonly
+<b>x52cli</b> - Command line interface to libx52
+\endhtmlonly
+
+# SYNOPSIS
+<tt>\b x52cli \a commands [\a command-options]</tt>
+
+# DESCRIPTION
+
+\b x52cli is a command line interface to the X52 library that allows you to set
+the LEDs and different parameters on the multifunction display (MFD).
+
+Running \b x52cli without any arguments will display a brief help message.
+
+# COMMANDS
+
+Commands are not case sensitive.
+
+- <tt>\b bri { \b mfd | \b led } < \a brightness >
+  </tt>\n \manonly \fR \endmanonly
+  Set the brightness of the \b MFD or <b>LED</b>s. \a brightness can be any
+  numeric value between 0 and 128. Higher values are accepted, but may not have
+  the desired effect.
+
+- <tt>\b mfd < \a line > < \a text > </tt>\n \manonly \fR \endmanonly
+  Set the text on the MFD \a line. \a line can be \c 0, \c 1 or \c 2, and refers
+  to the first, second or third line of the multifunction display respectively.
+  \a text cannot have embedded NUL characters (0x00) and must correspond with
+  the character map fo the MFD. \a text should be quoted in order to preserve
+  embedded whitespace. To pass raw hex values, use \b printf(1) as shown in the
+  \ref x52cli_examples section. Note that \a text is limited to a length of 16
+  characters. While you can pass in longer strings, they will be silenty
+  truncated.
+
+- <tt>\b led < \a led-id > < \a state > </tt>\n \manonly \fR \endmanonly
+  Set the LED \a led-id to \a state. See \ref x52cli_leds for a list of
+  supported values.
+
+- <tt>\b blink { \b on | \b off } </tt>\n \manonly \fR \endmanonly
+  Turn the \b blink state \b on or \b off.
+
+- <tt>\b shift { \b on | \b off } </tt>\n \manonly \fR \endmanonly
+  Turn the \b shift indicator in the MFD \b on or \b off.
+
+- <tt>\b clock { \b local | \b gmt } { \b 12hr | \b 24hr }
+  { \b ddmmyy | \b mmddyy | \b yymmdd }</tt>\n \manonly \fR \endmanonly
+  Set the clock 1 display to the current \b local or \b GMT time and date.
+  Clock can be configured to display in either \b 12hr or \b 24hr mode. Date
+  can be displayed in one of the following formats: \b DD-MM-YY, \b MM-DD-YY,
+  or \b YY-MM-DD.
+
+- <tt>\b offset { \b 2 | \b 3 } < \a offset-val >
+  { \b 12hr | \b 24hr }</tt>\n \manonly \fR \endmanonly
+  Set the offset for clock \b 2 or \b 3 and configure them to display in either
+  \b 12hr or \b 24hr mode. \a offset-val is in minutes east of \b UTC and can
+  range from \a -1440 to \a +1440.
+
+- <tt>\b time < \a hour > < \a minute >
+  { \b 12hr | \b 24hr }</tt>\n \manonly \fR \endmanonly
+  Set the time for clock 1 to <em>hour:minute</em> and configure it to display
+  in \b 12hr or \b 24hr mode.
+
+- <tt>\b date <\a dd > <\a mm > <\a yy>
+  { \b ddmmyy | \b mmddyy | \b yymmdd }</tt>\n \manonly \fR \endmanonly
+  Set the date on the MFD to the values represented by \a dd, \a mm and \a yy in
+  the requested format.
+
+- <tt>\b raw < \a wIndex > < \a wValue ></tt>\n \manonly \fR \endmanonly
+  Send a raw vendor control request to the connected joystick.\n
+  \warning You should only use this if you know what you are doing. Sending
+  an invalid control sequence can potentially destroy your device.
+
+@section x52cli_leds LEDs
+This is the list of LED IDs and corresponding states supported by the X52 Pro.
+Note that the \b on state is only allowed for the \b fire and \b throttle LEDs,
+and they do not support the \b red, \b amber and \b green states. The remaining
+LEDs do not support the \b on state, but support all the other states.
+
+\note The \b led command is only supported on the X52 Pro, not on the X52.
+
+## LED IDs
+
+- \b fire
+- \b a
+- \b b
+- \b d
+- \b e
+- \b t1
+- \b t2
+- \b t3
+- \b pov
+- \b clutch
+- \b throttle
+
+## States
+
+- \b off
+- \b on
+- \b red
+- \b amber
+- \b green
+
+# LIMITATIONS
+
+\b x52cli does not maintain any state between invocations. As a result the
+\b clock command will reset the relative offsets for clocks 2 and 3 back to 0
+and configure them to be in 12 hour mode. To work around this, use the \b date
+and \b time commands instead to manually configure the date and time.
+
+# PERMISSIONS
+
+You must have write permissions to the USB device in order to use the \b libx52
+library, and by extension, \b x52cli.
+
+The simplest method to obtain such permissions is to run \b x52cli as root,
+possibly through \b sudo(8)
+
+@section x52cli_examples EXAMPLES
+
+- Turn off the T1 LED.
+  > <tt>\b x52cli led t1 off</tt>
+
+- Turn the B LED to Amber.
+  > <tt>\b x52cli led B amber</tt>
+
+- Set line 1 of the MFD to display "Hello World"
+  > <tt>\b x52cli mfd 0 "Hello World"</tt>
+
+- Set line 2 of the MFD to display "¿Cómo Estás?"
+  > <tt>\b x52cli mfd 1 "$(printf '\\x9FC\\xE2mo Est\\xE0s?')"</tt>
+
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
