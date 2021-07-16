8545e28d094a62d0506663b53463b3010afcef9c
nirenjan <nirenjan@gmail.com>
2021-07-15 19:31:46 -0700

    Build and install inih manually on macOS

--- .github/scripts/install-dependencies-macos.sh
+++ .github/scripts/install-dependencies-macos.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 # Install dependencies to build and test on Ubuntu runners
 brew install \
     autoconf \
@@ -9,9 +9,17 @@ brew install \
     gettext \
     libusb \
     hidapi \
-    inih \
     doxygen \
     rsync \
+    meson \
     cmocka
+
+# inih cannot be installed via Homebrew, install it manually
+INIH_VER=r53
+curl -LO https://github.com/benhoyt/inih/archive/refs/tags/${INIH_VER}.tar.gz
+tar xvf ${INIH_VER}.tar.gz
+cd inih-${INIH_VER}
+meson build
+meson install -C build
 
 exit 0
