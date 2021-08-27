1174f7f1c46227fa2cfcf4b4fc5928388884b775
nirenjan <nirenjan@gmail.com>
2021-08-26 22:22:39 -0700

    Bail out if there are changes after make distcheck

--- .github/scripts/build-and-test.sh
+++ .github/scripts/build-and-test.sh
@@ -9,3 +9,9 @@ cd build
 make -j V=0
 make -j check V=0
 make -j distcheck
+
+# Make sure that there are no changes to the source code
+# This may happen if the source have changed with differences to the
+# translation files and templates. Enabling this will allow us to catch
+# missing/modified translations.
+git diff --exit-code
