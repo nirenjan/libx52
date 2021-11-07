9078ff08c6a308ef2d3f2e63bd6fe2200aede6de
nirenjan <nirenjan@users.noreply.github.com>
2021-11-07 15:56:48 +0000

    deploy: 87bf5881dd5d3d6c65929ce44082fc7ddee95d62

--- search/all_a.js
+++ search/all_a.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['x52_20driver_20daemon_177',['X52 driver daemon',['../x52d.html',1,'']]],
   ['x52d_5fdial_5fcommand_178',['x52d_dial_command',['../group__x52dcomm.html#ga59b382427afc417499311336747fb191',1,'x52dcomm.h']]],
-  ['x52d_5fsend_5fcommand_179',['x52d_send_command',['../group__x52dcomm.html#gad72175611919fcafc0a0533b7a01f167',1,'x52dcomm.h']]],
+  ['x52d_5fsend_5fcommand_179',['x52d_send_command',['../group__x52dcomm.html#gaef9b481c4e5faaac14e2681cb64c3faa',1,'x52dcomm.h']]],
   ['x52dcomm_2eh_180',['x52dcomm.h',['../x52dcomm_8h.html',1,'']]]
 ];
