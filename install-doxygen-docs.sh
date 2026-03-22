842e7e53ed74032810b3c12760c201536e917e5e
nirenjan <nirenjan@gmail.com>
2026-03-22 15:59:35 -0700

    fix: Scope the list of man pages installed
    The previous commit to address #63 added a number of extra unwanted man
    pages. This commit addresses that and scopes down the actual installed
    man pages to only x52cli and x52bugreport.
    
    GH-Issue: #63
    GH-Issue-URL: https://github.com/nirenjan/libx52/issues/63

--- install-doxygen-docs.sh
+++ install-doxygen-docs.sh
@@ -6,12 +6,20 @@ set -e
 doc_html="$1"
 mandir="$2"
 
+WANTED_PAGES="man1/x52cli.1 man1/x52bugreport.1"
+
 if [ -d "$MESON_BUILD_ROOT/docs/html" ]; then
   mkdir -p "$MESON_INSTALL_DESTDIR_PREFIX/$doc_html"
   cp -R "$MESON_BUILD_ROOT/docs/html"/. "$MESON_INSTALL_DESTDIR_PREFIX/$doc_html/"
 fi
 
 if [ -d "$MESON_BUILD_ROOT/docs/man" ]; then
-  mkdir -p "$MESON_INSTALL_DESTDIR_PREFIX/$mandir"
-  cp -R "$MESON_BUILD_ROOT/docs/man"/. "$MESON_INSTALL_DESTDIR_PREFIX/$mandir/"
+  MANDIR="$MESON_INSTALL_DESTDIR_PREFIX/$mandir"
+  mkdir -p "$MANDIR"
+  for manpage in $WANTED_PAGES
+  do
+    section=$(dirname "$manpage")
+    mkdir -p "$MANDIR/$section"
+    cp "$MESON_BUILD_ROOT/docs/man/$manpage" "$MANDIR/$section"
+  done
 fi
